bba1e128639baaa64e00a7110986601d728b20f3
tonic-6101 <tonicxx341@gmail.com>
2023-04-30 19:08:26 +0200

    refactored

--- src/store/index.js
+++ src/store/index.js
@@ -1,45 +1,9 @@
 // import { createStore } from 'redux';
 
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialCounterState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++; // Mute the state is allowed because of Redux using Lmer package, that clones the state.
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      // state.counter = state.counter + action.amount;
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: 'authentication',
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import counterReducer from './counter';
+import authReducer from './auth';
 
 // const counterReducer = (state = initialState, action) => {
 //   if (action.type === 'increment') {
@@ -81,13 +45,11 @@ const authSlice = createSlice({
 // const store = createStore(counterSlice.reducer); // Note: it can be only one reducer to createStore
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counterReducer, auth: authReducer },
   // reducer: {counter: counterSlice.reducer} // multiple reducers
 });
 
 // Methods automaticlly created by Redux Toolkit
 // the key will create action objects
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
